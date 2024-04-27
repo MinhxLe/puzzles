ea2b2841be2de2d36fdd84e15e36d5197e20057e
Minh Le <minh.d.le27@gmail.com>
2024-04-27 10:45:26 -0700

    qs

--- polygon.py
+++ polygon.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import math
 from functools import cached_property
-from typing import Counter
+from typing import Counter, Tuple
 
 
 @dataclass
@@ -19,11 +19,48 @@ def partition_polygon(idxs: list[int], vertices: list[int]) -> list[list[int]]:
     ]
 
 
-# write a polygon class
+@dataclass(frozen=True)
+class Polygon:
+    """
+    polygon defined by edges between set of vertices labeled by int on a N regular polyong
+    """
+
+    vertices: list[int]
+    N: int
+
+    def __post_init__(self):
+        assert len(set(self.vertices)) == len(self.vertices)
+        self.vertices.sort()
+
+    def canonical_form(self) -> Tuple[set[int], int]:
+        # sort vertices, index at 0
+        min_vertex = min(self.vertices)
+        return (set([(v - min_vertex) for v in self.vertices]), self.N)
+
+    def __hash__(self):
+        vertices, N = self.canonical_form()
+
+        return hash((tuple(vertices), N))
+
+    def partition(self, i: int, j: int) -> list["Polygon"]:
+        """
+        partition polygon with a segment from ith vertex to jth vertex
+        """
+        i_idx = self.vertices.index(i)
+        j_idx = self.vertices.index(j)
+        i_idx, j_idx = min(i_idx, j_idx), max(i_idx, j_idx)
+        return [
+            Polygon(self.vertices[i_idx : j_idx + 1], self.N),
+            Polygon(self.vertices[: i_idx + 1] + self.vertices[j_idx:], self.N),
+        ]
 
 
 @dataclass
-class PolygonProblem:
+class Utils:
+    """
+    utils scoped to a N regular unit polygon
+    """
+
     N: int
 
     @cached_property
@@ -52,7 +89,7 @@ class PolygonProblem:
         # law of cosine
         return math.sqrt((2 * self.R**2) - (2 * self.R**2 * math.cos(alpha)))
 
-    def get_area(self, vertices: list[int]) -> float:
+    def get_triangle_area(self, vertices: list[int]) -> float:
         assert len(vertices) == 3
         assert all([n < self.N for n in vertices])
         # heron's formula
@@ -64,12 +101,81 @@ class PolygonProblem:
         s = sum(d) / 2
         return math.sqrt(s * (s - d[0]) * (s - d[1]) * (s - d[2]))
 
-    def find_valid_count(self, max_area: float, vertices: list[int]) -> int:
+
+@dataclass
+class MaxTriangleCounter(Utils):
+    N: int
+
+    def _get_valid_combo_counts(self, max_area: float, polygon: Polygon, cache) -> int:
+        if polygon in cache:
+            return cache[polygon]
+        vertices = polygon.vertices
+        if len(vertices) < 3:
+            return 1
+        elif len(vertices) == 3:
+            area = self.get_triangle_area(vertices)
+            if not math.isclose(area, max_area) and area < max_area:
+                count = 1
+            else:
+                count = 0
+        else:
+            count = 0
+            for i in range(len(vertices)):
+                for dist in range(2, len(vertices) - 1):  # exclude adjacent vertices
+                    j = (i + dist) % len(vertices)
+                    p1, p2 = polygon.partition(vertices[i], vertices[j])
+                    count += self._get_valid_combo_counts(
+                        max_area, p1, cache
+                    ) * self._get_valid_combo_counts(max_area, p2, cache)
+            cache[polygon] = count
+        return count
+
+    def get_max_triangle_count(self, idxs: list[int]) -> int:
+        assert len(idxs) == 3
+        full_polygon = Polygon(list(range(self.N)), self.N)
+        area = self.get_triangle_area(idxs)
+        idxs.sort()
+        i, j, k = idxs
+        p1, r1 = full_polygon.partition(i, j)
+        p2, r2 = r1.partition(j, k)
+        # remaining is the triangle
+        r3, p3 = r2.partition(i, k)
+        assert r3.vertices == idxs
+        cache = dict()
+        return (
+            self._get_valid_combo_counts(area, p1, cache)
+            * self._get_valid_combo_counts(area, p2, cache)
+            * self._get_valid_combo_counts(area, p3, cache)
+        )
+
+    def get_all_max_triangle_counts(self) -> Tuple[Counter, int]:
+        # without loss of generality, we pick 1 vertices to be 0
+        counts = Counter()
+        i = 0
+        for j in range(1, self.N - 1):
+            for k in range(j + 1, self.N):
+                triangle = [i, j, k]
+                counts[tuple(triangle)] = self.get_max_triangle_count(triangle)
+        # every vertex has the same counts but every count is triple counted
+        # since it takes 3 vertices to make a get_triangle
+        total_count = sum(counts.values()) * self.N
+        assert total_count % 3 == 0
+
+        return counts, total_count // 3
+
+
+@dataclass
+class PolygonProblem(Utils):
+    N: int
+
+    def find_valid_count(
+        self, max_area: float, vertices: list[int], seen_partition: set[Tuple[int, int]]
+    ) -> int:
         vertices.sort()
         if len(vertices) < 3:
             return 1
         elif len(vertices) == 3:
-            area = self.get_area(vertices)
+            area = self.get_triangle_area(vertices)
             if not math.isclose(area, max_area) and area < max_area:
                 return 1
             else:
@@ -80,21 +186,24 @@ class PolygonProblem:
             for i in range(len(vertices)):
                 for dist in range(2, len(vertices) - 1):  # exclude adjacent vertices
                     j = (i + dist) % len(vertices)
-                    p1, p2 = partition_polygon([i, j], vertices)
-                    count += self.find_valid_count(
-                        max_area, p1
-                    ) * self.find_valid_count(max_area, p2)
-            # im severly double counting
-            return count // (2 * (len(vertices) - 3))
+                    if (i, j) not in seen_partition:
+                        seen_partition.add((i, j))
+                        p1, p2 = partition_polygon([i, j], vertices)
+                        count += self.find_valid_count(
+                            max_area,
+                            p1,
+                            seen_partition,
+                        ) * self.find_valid_count(max_area, p2, seen_partition)
+            return count
 
     def find_max_triangle_count(self, triangle: list[int]) -> int:
-        max_area = self.get_area(triangle)
+        max_area = self.get_triangle_area(triangle)
         p1, p2, p3 = partition_polygon(triangle, list(range(self.N)))
         return math.prod(
             [
-                self.find_valid_count(max_area, p1),
-                self.find_valid_count(max_area, p2),
-                self.find_valid_count(max_area, p3),
+                self.find_valid_count(max_area, p1, set()),
+                self.find_valid_count(max_area, p2, set()),
+                self.find_valid_count(max_area, p3, set()),
             ]
         )
 
--- test_polygon.py
+++ test_polygon.py
@@ -1,115 +1,159 @@
-from polygon import PolygonProblem, partition_polygon
+from polygon import Polygon, Utils, MaxTriangleCounter, Utils
 import math
 
 
-def test_get_num_edges_triangle():
-    # triangle
-    assert PolygonProblem(3).get_num_edges(0, 0) == 0
-    assert PolygonProblem(3).get_num_edges(0, 1) == 1
-    assert PolygonProblem(3).get_num_edges(0, 2) == 1
-    assert PolygonProblem(3).get_num_edges(1, 2) == 1
+class TestMaxTriangleCounter:
+    def test_get_max_triangle_count_square(self):
+        counter = MaxTriangleCounter(4)
+        assert counter.get_max_triangle_count([0, 1, 2]) == 0
+        assert counter.get_max_triangle_count([0, 2, 3]) == 0
+        assert counter.get_max_triangle_count([0, 3, 1]) == 0
+
+    def test_get_max_triangle_count_pentagon(self):
+        counter = MaxTriangleCounter(5)
+        assert counter.get_max_triangle_count([0, 1, 2]) == 0
+        # isoles triangle
+        assert counter.get_max_triangle_count([0, 1, 3]) == 1
+        assert counter.get_max_triangle_count([1, 4, 2]) == 1
+
+    def test_get_max_triangle_count_hexagon(self):
+        counter = MaxTriangleCounter(6)
+        assert counter.get_max_triangle_count([0, 1, 2]) == 0
+        assert counter.get_max_triangle_count([0, 2, 3]) == 0
+        # equilateral triangle
+        assert counter.get_max_triangle_count([0, 2, 4]) == 1
+        assert counter.get_max_triangle_count([1, 3, 5]) == 1
+
+    def test_get_all_4(self):
+        _, count = MaxTriangleCounter(4).get_all_max_triangle_counts()
+        assert count == 0
+
+    def test_get_all_5(self):
+        _, count = MaxTriangleCounter(5).get_all_max_triangle_counts()
+        assert count == 5
+
+    def test_get_all_6(self):
+        _, count = MaxTriangleCounter(6).get_all_max_triangle_counts()
+        assert count == 2
+
+    def test_get_all_8(self):
+        _, count = MaxTriangleCounter(8).get_all_max_triangle_counts()
+        assert count == 384
+
+    def test_get_all_10(self):
+        _, count = MaxTriangleCounter(10).get_all_max_triangle_counts()
+        assert count == 91520
+
+    def test_get_all_100(self):
+        _, count = MaxTriangleCounter(100).get_all_max_triangle_counts()
+        assert count == 0
 
 
-def test_get_num_edges_offset():
-    assert PolygonProblem(10).get_num_edges(3, 6) == 3
+class TestPolygon:
+    def test_canonical_form(self):
+        assert Polygon([1, 2], 3).canonical_form() == ({0, 1}, 3)
+        assert Polygon([1, 3], 3).canonical_form() == ({0, 2}, 3)
+
+        assert Polygon([1, 3, 5], 8).canonical_form() == ({0, 2, 4}, 8)
+
+    def test_partition(self):
+        assert Polygon([1, 2, 4, 5], 8).partition(2, 5) == [
+            Polygon([2, 4, 5], 8),
+            Polygon([2, 5, 1], 8),
+        ]
+
+        assert Polygon([1, 2, 4, 5], 8).partition(4, 2) == [
+            Polygon([2, 4], 8),
+            Polygon([2, 5, 4, 1], 8),
+        ]
+
+    def test_partition_boundary(self):
+        assert Polygon([1, 2, 4, 5], 8).partition(1, 2) == [
+            Polygon([1, 2], 8),
+            Polygon([1, 2, 4, 5], 8),
+        ]
 
 
-def test_get_num_edges_ordering():
-    assert PolygonProblem(10).get_num_edges(6, 3) == 3
+class TestUtils:
+    def test_get_num_edges_triangle(self):
+        # triangle
+        assert Utils(3).get_num_edges(0, 0) == 0
+        assert Utils(3).get_num_edges(0, 1) == 1
+        assert Utils(3).get_num_edges(0, 2) == 1
+        assert Utils(3).get_num_edges(1, 2) == 1
+
+    def test_get_num_edges_offset(self):
+        assert Utils(10).get_num_edges(3, 6) == 3
+
+    def test_get_num_edges_ordering(self):
+        assert Utils(10).get_num_edges(6, 3) == 3
+
+    def test_get_num_edges_other_direction(self):
+        assert Utils(10).get_num_edges(0, 7) == 3
+
+    def test_get_distance_square(self):
+        square = Utils(4)
+        assert math.isclose(square.get_distance(0, 1), 1)
+        assert math.isclose(square.get_distance(0, 2), math.sqrt(2))
+        assert math.isclose(square.get_distance(0, 3), 1)
+        assert math.isclose(square.get_distance(1, 1), 0)
+
+    def test_get_distance_hexagon(self):
+        hexagon = Utils(6)
+        assert math.isclose(hexagon.get_distance(0, 1), 1)
+        R = (2 * math.sin(math.pi / 6)) ** -1
+        assert math.isclose(
+            hexagon.get_distance(0, 2),
+            math.sqrt(2 * R**2 - 2 * R**2 * math.cos(2 * math.pi / 3)),
+        )
+        assert math.isclose(hexagon.get_distance(0, 3), 2 * R)
+
+    def test_get_triangle_area_square(self):
+        square = Utils(4)
+        assert math.isclose(square.get_triangle_area([0, 1, 2]), 0.5)
+        assert math.isclose(square.get_triangle_area([0, 1, 1]), 0)
+
+    def test_get_triangle_area_hexagon(self):
+        hexagon = Utils(6)
+        assert math.isclose(hexagon.get_triangle_area([0, 2, 2]), 0)
+
+        # area of hexagon / 2
+        assert math.isclose(hexagon.get_triangle_area([0, 2, 4]), 3 * math.sqrt(3) / 4)
+
+        assert math.isclose(hexagon.get_triangle_area([0, 3, 4]), math.sqrt(3) / 2)
 
 
-def test_get_num_edges_other_direction():
-    assert PolygonProblem(10).get_num_edges(0, 7) == 3
-
-
-def test_get_distance_square():
-    square = PolygonProblem(4)
-    assert math.isclose(square.get_distance(0, 1), 1)
-    assert math.isclose(square.get_distance(0, 2), math.sqrt(2))
-    assert math.isclose(square.get_distance(0, 3), 1)
-    assert math.isclose(square.get_distance(1, 1), 0)
-
-
-def test_get_distance_hexagon():
-    hexagon = PolygonProblem(6)
-    assert math.isclose(hexagon.get_distance(0, 1), 1)
-    R = (2 * math.sin(math.pi / 6)) ** -1
-    assert math.isclose(
-        hexagon.get_distance(0, 2),
-        math.sqrt(2 * R**2 - 2 * R**2 * math.cos(2 * math.pi / 3)),
-    )
-    assert math.isclose(hexagon.get_distance(0, 3), 2 * R)
-
-
-def test_get_area_square():
-    square = PolygonProblem(4)
-    assert math.isclose(square.get_area([0, 1, 2]), 0.5)
-    assert math.isclose(square.get_area([0, 1, 1]), 0)
-
-
-def test_get_area_hexagon():
-    hexagon = PolygonProblem(6)
-    assert math.isclose(hexagon.get_area([0, 2, 2]), 0)
-
-    # area of hexagon / 2
-    assert math.isclose(hexagon.get_area([0, 2, 4]), 3 * math.sqrt(3) / 4)
-
-    assert math.isclose(hexagon.get_area([0, 3, 4]), math.sqrt(3) / 2)
-
-
-def test_partition_polygon():
-    assert partition_polygon([0, 1], list(range(4))) == [[0, 1], [0, 1, 2, 3]]
-    assert partition_polygon([0, 2], list(range(4))) == [[0, 1, 2], [0, 2, 3]]
-    assert partition_polygon([0, 3], list(range(4))) == [[0, 1, 2, 3], [0, 3]]
-
-    assert partition_polygon([0, 2, 4], list(range(6))) == [
-        [0, 1, 2],
-        [2, 3, 4],
-        [0, 4, 5],
-    ]
-
-    assert partition_polygon([0, 1, 4], list(range(6))) == [
-        [0, 1],
-        [1, 2, 3, 4],
-        [0, 4, 5],
-    ]
-
-    assert partition_polygon([0, 1], [0, 2, 4, 5]) == [[0, 2], [0, 2, 4, 5]]
-
-    assert partition_polygon([1, 4], [4, 5, 6, 7, 8, 9]) == [[5, 6, 7, 8], [4, 5, 8, 9]]
-
-
-def test_find_max_triangle_count_octogon():
-    poly = PolygonProblem(8)
-    assert poly.find_max_triangle_count([0, 2, 4]) == 4
-    assert poly.find_max_triangle_count([0, 2, 5]) == 4
-
-
-def test_find_all_max_triangle_count_square():
-    square = PolygonProblem(4)
-    counter = square.find_all_max_triangle_count()
-    assert sum(counter.values()) == 0
-
-
-def test_find_all_max_triangle_count_hexagon():
-    hex = PolygonProblem(6)
-    counter = hex.find_all_max_triangle_count()
-    assert sum(counter.values()) == 1
-
-
-def test_find_all_max_triangle_count_pentagon():
-    poly = PolygonProblem(5)
-    counter = poly.find_all_max_triangle_count()
-    for triangle, count in counter.items():
-        if count > 0:
-            print(f"{triangle} {count}")
-    assert sum(counter.values()) == 3
-
-
-def test_find_all_max_triangle_count_big():
-    poly = PolygonProblem(444)
-    counter = poly.find_all_max_triangle_count()
-    for triangle, count in counter.items():
-        if count > 0:
-            print(f"{triangle} {count}")
-    assert sum(counter.values()) == 3
+# def test_find_max_triangle_count_octogon():
+#     poly = PolygonProblem(8)
+#     assert poly.find_max_triangle_count([0, 2, 4]) == 4
+#     assert poly.find_max_triangle_count([0, 2, 5]) == 4
+#
+#
+# def test_find_all_max_triangle_count_square():
+#     square = PolygonProblem(4)
+#     counter = square.find_all_max_triangle_count()
+#     assert sum(counter.values()) == 0
+#
+#
+# def test_find_all_max_triangle_count_hexagon():
+#     hex = PolygonProblem(6)
+#     counter = hex.find_all_max_triangle_count()
+#     assert sum(counter.values()) == 1
+#
+#
+# def test_find_all_max_triangle_count_pentagon():
+#     poly = PolygonProblem(5)
+#     counter = poly.find_all_max_triangle_count()
+#     for triangle, count in counter.items():
+#         if count > 0:
+#             print(f"{triangle} {count}")
+#     assert sum(counter.values()) == 3
+#
+#
+# def test_find_all_max_triangle_count_big():
+#     poly = PolygonProblem(444)
+#     counter = poly.find_all_max_triangle_count()
+#     for triangle, count in counter.items():
+#         if count > 0:
+#             print(f"{triangle} {count}")
+#     assert sum(counter.values()) == 3
